7df6377c350876637947de914d44e97df0f4d545
darkliself <4e486bux@gmail.com>
2022-02-10 14:28:18 +0200

    Initial commit

---
+++ app/src/main/java/com/darkliself/datastoretest/MainScreen.kt
@@ -0,0 +1,77 @@
+package com.darkliself.datastoretest
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun MainScreen() {
+    var key by remember { mutableStateOf("") }
+    var value by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box(
+            Modifier
+                .fillMaxWidth(0.8f)
+                .background(MaterialTheme.colors.onSurface.copy(alpha = 0.12f)),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Enter key")
+                BasicTextField(
+                    modifier = Modifier.fillMaxWidth(0.8f),
+                    value = key, onValueChange = {
+                        key = it
+                    })
+            }
+        }
+        Box(
+            Modifier
+                .fillMaxWidth(0.8f)
+                .background(MaterialTheme.colors.onSurface.copy(alpha = 0.12f)),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Enter value")
+
+                BasicTextField(
+                    modifier = Modifier.fillMaxWidth(0.8f),
+                    value = value, onValueChange = {
+                        value = it
+                    })
+            }
+        }
+
+        Button(
+            onClick = { /*TODO*/ }) {
+            Text("SAVE")
+        }
+
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultPreview() {
+    MainScreen()
+}
